6ff624053d0b6cee6decc6bb1e994b035dcb0255
Rehan Manzoor <rehan_manzoor@outlook.com>
2019-02-04 16:12:08 +0400

    Completed electronic shop

---
+++ implementation/electronics-shop.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import sys
+
+
+def getMoneySpent(keyboards, drives, s):
+    keyboards = sorted(keyboards, reverse=True)
+    drives = sorted(drives, reverse=True)
+
+    min = -1
+    for i in range(0, len(keyboards)):
+        for j in range(0, len(drives)):
+            sum = keyboards[i] + drives[j]
+
+            if sum <= s and sum > min:
+                min = sum
+
+    return min
+
+
+s, n, m = '10 2 3'.split(' ')
+s, n, m = [int(s), int(n), int(m)]
+
+keyboards = list(map(int, '3 1'.strip().split(' ')))
+drives = list(map(int, '5 2 8'.strip().split(' ')))
+
+moneySpent = getMoneySpent(keyboards, drives, s)
+
+print(moneySpent)
