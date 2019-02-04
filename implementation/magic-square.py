c3f038bd22b4b82b7f4db4734330702cf27225e8
Rehan Manzoor <rehan_manzoor@outlook.com>
2019-02-04 19:07:10 +0400

    working with magic square

--- implementation/magic-square.py
+++ implementation/magic-square.py
@@ -3,48 +3,102 @@
 # !/bin/python3
 # This solution is still incomplete
 
+def check_sum(s):
+    x1 = s[0][0] + s[0][1] + s[0][2]
+    x2 = s[1][0] + s[1][1] + s[1][2]
+    x3 = s[2][0] + s[2][1] + s[2][2]
+
+    y1 = s[0][0] + s[1][0] + s[2][0]
+    y2 = s[0][1] + s[1][1] + s[2][1]
+    y3 = s[0][2] + s[1][2] + s[2][2]
+
+    return x1 == x2 and x2 == x3 and x3 == y1 and y1 == y2 and y2 == y3
+
+
 def is_magical(s):
     arr = [True] * 9
+    fixes = []
+    repeat = [0] * 9
 
     # center = '1,1'  should be 5
+    # edge should be even numbers
+    # center sides should be odd numbers
 
-    if s[1][1] is not 5:
-        return False
+    for i in range(0, 3):
+        for j in range(0, 3):
 
-    arr[5 - 1] = False
+            repeat[s[i][j] - 1] += 1
 
-    edges = ['0,0', '0,2', '2,0', '2,2']  # should be even
+            if i is 1 and j is 1 and s[i][j] is not 5:
+                fixes.append([1, 1, s[1][1]])
+                continue
+            elif i is 1 and j is 1 and s[i][j] is 5:
+                arr[s[i][j] - 1] = False
+                continue
 
-    for edge in edges:
-        i, j = edge.split(',')
-        i = int(i)
-        j = int(j)
-        if s[i][j] % 2 != 0:
-            return False
+            if s[i][j] % 2 != ((i + j) % 2):
+                fixes.append([i, j, s[i][j]])
+                continue
 
-        arr[s[i][j] - 1] = False
+            arr[s[i][j] - 1] = False
 
-    side_centers = ['0,1', '1,0', '1,2', '2,1']  # should be odd
-    for side_center in side_centers:
-        i, j = side_center.split(',')
-        i = int(i)
-        j = int(j)
-        if s[i][j] % 2 != 1:
-            return False
+    if len(fixes) > 0:
+        return fixes, arr, repeat
 
-        arr[s[i][j] - 1] = False
+    magical = check_sum(s)
 
-    return True not in arr
+    return fixes, magical, magical
+
+
+def apply_fixes(s, fixes, vals, repeat):
+    misplaced = [(i + 1) for i, v in enumerate(vals) if v is True]
+    repeat = [(index + 1) for index, val in enumerate(repeat) if val > 1]
+
+    print(fixes)
+    print(misplaced)
+    print(repeat)
 
 
 def get_cost(s):
-    magical = is_magical(s)
+    v = False
 
-    print(magical)
+    while v is not True:
+        fixes, v, repeat = is_magical(s)
+
+        if v is True:
+            return 0
+
+        if v is False:
+            print(fixes)
+            break
+
+        s = apply_fixes(s, fixes, v, repeat)
+        break
 
 
-s = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
-get_cost(s)
-
+'''
 s = [[4, 9, 2], [3, 5, 7], [8, 1, 7]]
-get_cost(s)
+cost = get_cost(s)
+print('COST: {}'.format(cost))
+print('\r\n================\r\n')
+
+s = [[4, 9, 2], [4, 5, 7], [8, 1, 7]]
+cost = get_cost(s)
+print('COST: {}'.format(cost))
+print('\r\n================\r\n')
+
+s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+cost = get_cost(s)
+print('COST: {}'.format(cost))
+print('\r\n================\r\n')
+'''
+
+s = [[2, 1, 4], [7, 5, 3], [8, 9, 6]]
+cost = get_cost(s)
+print('COST: {}'.format(cost))
+
+s = [[2, 9, 4], [7, 5, 3], [8, 1, 6]] # 9 - 1
+
+s = [[2, 9, 4], [3, 5, 7], [8, 1, 6]] # 3 - 7
+
+s = [[4, 9, 2], [3, 5, 7], [8, 1, 6]] # 4 - 2
